29dc20c265c63f22a42b6c01f814f3fc7fa15ad0
PiyushChall <115055657+PiyushChall@users.noreply.github.com>
2023-06-27 16:34:48 +0530

    added game over scene and some gameplay stufs

--- Platformer/Assets/Scripts/PlyerMoves.cs
+++ Platformer/Assets/Scripts/PlyerMoves.cs
@@ -2,17 +2,119 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlyerMoves : MonoBehaviour
+public class CharecterController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float horizontal;
+    public float speed = 8f;
+    public float jumpingPower = 16f;
+    private bool isFacingRight = true;
+    public bool isJumping;
+    public float jumpTimeCounter;
+    public float jumpTime;
+
+    private bool canDash = true;
+    private bool isDashing;
+    public float dashingPower = 24f;
+    public float dashingTime = 0.2f;
+    public float dashingCooldown = 1f;
+    public float checkRadius = 0.2f;
+    private bool isGrounded;
+
+
+
+    [SerializeField] public Rigidbody2D rb;
+    [SerializeField] public Transform groundCheck;
+    [SerializeField] public LayerMask groundLayer;
+    [SerializeField] public TrailRenderer tr;
+
+
+
+    private void Update()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, groundLayer);
+        if (isDashing)
+        {
+            return;
+        }
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        if (isGrounded == true && Input.GetKeyDown(KeyCode.Space))
+        {
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpingPower;
+        }
+
+        if (Input.GetKeyDown(KeyCode.X) && canDash)
+        {
+            StartCoroutine(Dash());
+        }
+
+        if (Input.GetKey(KeyCode.Space) && isJumping == true)
+        {
+            if (jumpTimeCounter > 0)
+            {
+                rb.velocity = Vector2.up * jumpingPower;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            isJumping = false;
+        }
+
+
+
+        Flip();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (isDashing)
+        {
+            return;
+        }
+
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            Vector3 localScale = transform.localScale;
+            isFacingRight = !isFacingRight;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        isDashing = true;
+        float originalGravity = rb.gravityScale;
+        rb.gravityScale = 0f;
+        rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+        tr.emitting = true;
+        yield return new WaitForSeconds(dashingTime);
+        tr.emitting = false;
+        rb.gravityScale = originalGravity;
+        isDashing = false;
+        yield return new WaitForSeconds(dashingCooldown);
+        canDash = true;
     }
 }
+
